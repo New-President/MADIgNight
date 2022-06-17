9b2bd882aef6e09cfed4ea69699eb0ba3d8e42fb
Wee Kang Lee <s10222162@connect.np.edu.sg>
2022-06-17 15:34:50 +0800

    Added CreateBlogPostActivity

---
+++ app/src/main/java/sg/edu/np/ignight/CreateBlogActivity.java
@@ -0,0 +1,18 @@
+package sg.edu.np.ignight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateBlogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_blog);
+    }
+
+
+
+
+}
