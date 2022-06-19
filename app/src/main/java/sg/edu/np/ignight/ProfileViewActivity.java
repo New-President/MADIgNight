8002b89cf4b31b8f82600376c646161a4d2a2311
HanXihe <s10222998@connect.np.edu.sg>
2022-06-19 23:56:47 +0800

    - ProfileViewActivity.java with no profile picture

--- app/src/main/java/sg/edu/np/ignight/ProfileViewActivity.java
+++ app/src/main/java/sg/edu/np/ignight/ProfileViewActivity.java
@@ -1,5 +1,7 @@
 package sg.edu.np.ignight;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,13 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,104 +25,49 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileViewActivity extends AppCompatActivity {
     private String username;
     private String aboutMe;
     private String whatImLookingFor;
-    private String nameAndAgeJoined;
-    private String age;
-    private String stringInterests;
-    private ArrayList interests;
-    private HashMap userInfo;
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
+    private Integer age;
+    private String nameAndAge1;
     private TextView nameAndAge, textView8, textView9;
     private Button ignightButton;
     private ImageButton backButton;
+    public ArrayList<String> interestsDisplay;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
 
-        // gets the user UID here to identify which user it is
-        // current UID is for testing only
-        String uid = "SqDiaNh7KGhYd09lWeVpVrRTSKc2";
+        /*
+        // pass in userObject via putExtra intent
+        UserObject userObject = (UserObject) getIntent().getSerializableExtra("key"); */
 
-        // insert profile interest data here
-        database = FirebaseDatabase.getInstance("https://madignight-default-rtdb.asia-southeast1.firebasedatabase.app");
-        myRef = database.getReference("user").child(uid);
+        UserObject userObject = new UserObject();
+        userObject.setUsername("test");
+        userObject.setAboutMe("test2");
+        userObject.setAge(123);
+        userObject.setRelationshipPref("test3");
+        ArrayList<String> interestsDisplayTest = new ArrayList<>();
+        interestsDisplayTest.add("test1");
+        interestsDisplayTest.add("test2");
+        userObject.setInterestList(interestsDisplayTest);
 
-        // User profile data from the database will be extracted for display
-        // Interests are stored as children
-        // UID here will be extracted from the profile as a string
-        interests = new ArrayList();
+        myRef = FirebaseDatabase.getInstance("https://madignight-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("chat");
+        String currentUserUID = FirebaseAuth.getInstance().getUid();
+        String targetUserUID = userObject.getUid();
+
+
         ArrayList<String> interestsDisplay = new ArrayList<>();
+        interestsDisplay = userObject.getInterestList();
 
-        // profile information will be collected in a loop in onDataChange
-        userInfo = new HashMap();
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) { // extracts profile info
-                if(snapshot.exists()){
-                    for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                        String data = dataSnapshot.getKey().toString();
-                        String data1 = dataSnapshot.getValue().toString();
-                        userInfo.put(data, data1);
-                        Log.d("test", data1);
-                    }
-
-                    // userInfo contains all of the relevant data
-                    aboutMe = (String) userInfo.get("About Me");
-                    username = (String) userInfo.get("username");
-                    whatImLookingFor = (String) userInfo.get("Relationship Preference");
-                    age = (String) userInfo.get("Age");
-                    stringInterests = (String) userInfo.get("Interest");
-
-                    // converts stringInterests into array after converting string
-                    stringInterests = stringInterests.substring(1, stringInterests.length() - 1);
-                    interests = new ArrayList<String>(Arrays.asList(stringInterests.split(", ")));
-                    for (int i = 0; i < interests.size(); i++){
-                        String b = (String) interests.get(i);
-                        String a = b.substring(0, b.length() - 1);
-                        interestsDisplay.add(a);
-                        Log.d("test2", a);
-                    }
-
-                    // set text
-                    nameAndAge = (TextView) findViewById(R.id.NameAndAgeTextView);
-                    nameAndAgeJoined = username + ", " + age;
-                    nameAndAge.setText(nameAndAgeJoined);
-                    textView8 = (TextView) findViewById(R.id.textView8);
-                    textView8.setText(aboutMe);
-                    textView9 = (TextView) findViewById(R.id.textView9);
-                    textView9.setText(whatImLookingFor);
-                }
-                else{
-                    // if there is any issue, this toast error will popup
-                    Log.d("failed", "failed");
-                    Toast.makeText(getApplicationContext(),
-                            "Profile retrieval failed. Please try again later.",
-                            Toast.LENGTH_LONG)
-                            .show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // if there is any issue, this toast error will popup
-                Log.d("failed", "failed");
-                Toast.makeText(getApplicationContext(),
-                        "Profile retrieval failed. Please try again later.",
-                        Toast.LENGTH_LONG)
-                        .show();
-            }
-        });
-
+        ShowInformation(userObject);
 
         // add intents for bottom buttons here
         backButton = findViewById(R.id.profileViewBackButton);
@@ -134,10 +83,9 @@ public class ProfileViewActivity extends AppCompatActivity {
         ignightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent startChat = new Intent(getApplicationContext(), ChatActivity.class);
-                startActivity(startChat);
             }
         });
+
         RecyclerView rv = findViewById(R.id.InterestsRecyclerView);
         ProfileViewInterestsAdapter adapter = new ProfileViewInterestsAdapter(ProfileViewActivity.this, interestsDisplay);
         LinearLayoutManager layout = new LinearLayoutManager(this);
@@ -146,5 +94,25 @@ public class ProfileViewActivity extends AppCompatActivity {
 
         rv.setAdapter(adapter);
         rv.setLayoutManager(layout);
+    }
+
+    public void ShowInformation(UserObject userObject){
+        myRef = FirebaseDatabase.getInstance("https://madignight-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("chat");
+        String currentUserUID = FirebaseAuth.getInstance().getUid();
+        String targetUserUID = userObject.getUid();
+
+
+        username = (String) userObject.getUsername();
+        aboutMe = (String) userObject.getAboutMe();
+        whatImLookingFor = (String) userObject.getRelationshipPref();
+        age = (Integer) userObject.getAge();
+
+        nameAndAge1 = username + ", " + age.toString();
+        nameAndAge = (TextView) findViewById(R.id.NameAndAgeTextView);
+        nameAndAge.setText(nameAndAge1);
+        textView8 = (TextView) findViewById(R.id.textView8);
+        textView8.setText(aboutMe);
+        textView9 = (TextView) findViewById(R.id.textView9);
+        textView9.setText(whatImLookingFor);
     }
 }
--- app/src/main/java/sg/edu/np/ignight/UserObject.java
+++ app/src/main/java/sg/edu/np/ignight/UserObject.java
@@ -1,8 +1,9 @@
 package sg.edu.np.ignight;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class UserObject {
+public class UserObject implements Serializable {
     private String uid;
     private String email;
     private String phone;
