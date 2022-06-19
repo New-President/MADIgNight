55feda83964e314564c1ba7e7ad4b8000faebf05
HanXihe <s10222998@connect.np.edu.sg>
2022-06-19 15:19:06 +0800

    - Unlinked prrofileView - Remove RegistrationActivity.java

--- app/src/main/java/sg/edu/np/ignight/ProfileViewActivity.java
+++ app/src/main/java/sg/edu/np/ignight/ProfileViewActivity.java
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,38 +22,62 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class ProfileViewActivity extends AppCompatActivity {
+    private String username, aboutMe, whatImLookingFor, uid;
+    private Integer age;
+    private ArrayList interests;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
+    private User userData;
+    private Button friendButton, ignightButton;
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
 
-        ArrayList<User> data = new ArrayList<>(); // placeholder
         // insert profile interest data here
         database = FirebaseDatabase.getInstance("https://madignight-default-rtdb.asia-southeast1.firebasedatabase.app");
         myRef = database.getReference("user");
 
-        // Interest data from the database will be extracted.
+        // User profile data from the database will be extracted for display
         // Interests are stored as children
+        // UID here will be extracted from the profile as a string
+        userData = new User();
+        interests = new ArrayList();
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) { // extracts profile info
                 for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    if(dataSnapshot.child("email").getValue().equal)
+                    // extracts profile info
+                    username = (String) dataSnapshot.child("username").getValue();
+                    age = (Integer) dataSnapshot.child("Age").getValue();
+                    aboutMe = (String) dataSnapshot.child("About Me").getValue();
+                    whatImLookingFor = (String) dataSnapshot.child("Relationship Preference").getValue();
+
+                    // sets profile info to a user object, so that the info can be passed around the app
+                    userData.Username = username;
+                    userData.Age = age;
+                    userData.Aboutme = aboutMe;
+                    userData.Relationship_pref = whatImLookingFor;
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                // if there is any issue, this toast error will popup
+                Log.d("failed", "failed");
+                Toast.makeText(getApplicationContext(),
+                        "Profile retrival failed. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
             }
         });
 
 
         RecyclerView rv = findViewById(R.id.recyclerView);
-        ProfileViewInterestsAdapter adapter = new ProfileViewInterestsAdapter(data);
+        ProfileViewInterestsAdapter adapter = new ProfileViewInterestsAdapter(interests);
         LinearLayoutManager layout = new LinearLayoutManager(this);
         layout.setOrientation(LinearLayoutManager.HORIZONTAL);
 
@@ -55,6 +85,29 @@ public class ProfileViewActivity extends AppCompatActivity {
         rv.setLayoutManager(layout);
 
         // add intents for bottom buttons here
+        backButton = findViewById(R.id.BackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backToMainMenu = new Intent(getApplicationContext(), MainMenuActivity.this);
 
+            }
+        });
+
+        friendButton = findViewById(R.id.button);
+        friendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+        ignightButton = findViewById(R.id.createButton);
+        ignightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
     }
 }
