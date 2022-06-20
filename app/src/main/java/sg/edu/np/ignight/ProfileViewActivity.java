0ffbca0c8c256cf1641e9954af08bc8e6d78766f
HanXihe <s10222998@connect.np.edu.sg>
2022-06-20 10:16:03 +0800

    - Completed ProfileViewActivity.java

--- app/src/main/java/sg/edu/np/ignight/ProfileViewActivity.java
+++ app/src/main/java/sg/edu/np/ignight/ProfileViewActivity.java
@@ -3,19 +3,29 @@ package sg.edu.np.ignight;
 import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -23,7 +33,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -37,11 +52,26 @@ public class ProfileViewActivity extends AppCompatActivity {
     private String whatImLookingFor;
     private Integer age;
     private String nameAndAge1;
+    private String currentUserUID, targetUserUID;
+
     private TextView nameAndAge, textView8, textView9;
+
     private Button ignightButton;
     private ImageButton backButton;
+
+    private ImageView profilePicture;
+
     public ArrayList<String> interestsDisplay;
+
     private DatabaseReference myRef;
+    private FirebaseDatabase db;
+    private Map userMap;
+    private FirebaseAuth mAuth;
+
+    private Uri imageUri;
+    private String myUri = "";
+    private StorageReference storageProfilePic;
+    private UploadTask uploadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +79,7 @@ public class ProfileViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_view);
 
         /*
-        // pass in userObject via putExtra intent
+        // pass in userObject via putExtra intent here
         UserObject userObject = (UserObject) getIntent().getSerializableExtra("key"); */
 
         UserObject userObject = new UserObject();
@@ -62,15 +92,16 @@ public class ProfileViewActivity extends AppCompatActivity {
         interestsDisplayTest.add("test2");
         userObject.setInterestList(interestsDisplayTest);
 
-        myRef = FirebaseDatabase.getInstance("https://madignight-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("chat");
-        String currentUserUID = FirebaseAuth.getInstance().getUid();
-        String targetUserUID = userObject.getUid();
+        db = FirebaseDatabase.getInstance("https://madignight-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        myRef = db.getReference().child("chat");
+        storageProfilePic = FirebaseStorage.getInstance().getReference().child("profilePicture").child(userObject.getUid());
 
-
-        ArrayList<String> interestsDisplay = new ArrayList<>();
+        currentUserUID = FirebaseAuth.getInstance().getUid();
+        targetUserUID = userObject.getUid();
+        interestsDisplay = new ArrayList<>();
         interestsDisplay = userObject.getInterestList();
-
         ShowInformation(userObject);
+        setProfilePicture(userObject);
 
         // add intents for bottom buttons here
         backButton = findViewById(R.id.profileViewBackButton);
@@ -86,6 +117,48 @@ public class ProfileViewActivity extends AppCompatActivity {
         ignightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        boolean chatExists = false;
+                        ArrayList<String> usersInChat = new ArrayList<>();
+                        for (DataSnapshot chatIdSnapshot : snapshot.getChildren()) {
+                            for (DataSnapshot userIdSnapshot : chatIdSnapshot.child("users").getChildren()) {
+                                usersInChat.add(userIdSnapshot.getKey());
+                            }
+                            if (usersInChat.contains(getApplicationContext()) && usersInChat.contains(targetUserUID)) {
+                                chatExists = true;
+                                break;
+                            }
+                        }
+
+                        if (!chatExists) {
+                            String newChatID = myRef.push().getKey();
+                            userMap = new HashMap<>();
+                            userMap.put(currentUserUID, true);
+                            userMap.put(targetUserUID, true);
+
+                            myRef.child(newChatID).child("users").updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+                                @Override
+                                public void onComplete(@NonNull Task task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString("chatID", newChatID);
+                                        bundle.putString("chatName", userObject.getUsername());
+                                        bundle.putString("targetUserID", targetUserUID);
+                                        intent.putExtras(bundle);
+                                        view.getContext().startActivity(intent);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e(TAG, "onCancelled: " + error.getMessage());
+                    }
+                });
             }
         });
 
@@ -100,11 +173,6 @@ public class ProfileViewActivity extends AppCompatActivity {
     }
 
     public void ShowInformation(UserObject userObject){
-        myRef = FirebaseDatabase.getInstance("https://madignight-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("chat");
-        String currentUserUID = FirebaseAuth.getInstance().getUid();
-        String targetUserUID = userObject.getUid();
-
-
         username = (String) userObject.getUsername();
         aboutMe = (String) userObject.getAboutMe();
         whatImLookingFor = (String) userObject.getRelationshipPref();
@@ -117,5 +185,26 @@ public class ProfileViewActivity extends AppCompatActivity {
         textView8.setText(aboutMe);
         textView9 = (TextView) findViewById(R.id.textView9);
         textView9.setText(whatImLookingFor);
+
+
+    }
+
+    public void setProfilePicture(UserObject userObject){
+        profilePicture = (ImageView) findViewById(R.id.imageView);
+        final long img_bytes = 1024 * 1024 * 3;
+        storageProfilePic.getBytes(img_bytes).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                profilePicture.setImageBitmap(bmp);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(getApplicationContext(),
+                        "Error retrieving posts",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
