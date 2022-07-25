180106dcba53df6a6aa76c485f38cf939b63a20b
HanXihe-NP <cerohan45@gmail.com>
2022-07-25 21:53:10 +0800

    - Copied over a layout for quick testing - New fonts

--- app/src/main/java/sg/edu/np/ignight/ViewProfileCustomisation.java
+++ app/src/main/java/sg/edu/np/ignight/ViewProfileCustomisation.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ViewProfileCustomisation extends AppCompatActivity {
+public class ViewProfileCustomisation extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private ImageButton backButton3;
     private boolean customiseProfileBackground;
     private boolean customiseProfileFont;
     private boolean customiseProfileAccentTheme;
+    private Spinner fontSpinner, accentThemeSpinner ;
 
-    String[] FontDropdownOptions = {"Amaranth", "Cormorant", "Poppins", "Ropa", "Square Peg"};
+    // Spinner options for fonts
+    private String[] fontDropdownOptions = {"Amaranth", "Cormorant", "Poppins", "Ropa", "Square Peg"};
+    private String[] accentThemeDropdownOptions = {"Green", "IgNight Yellow", "Black", "Purple",
+            "Blue", "Red", "Brown"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +56,44 @@ public class ViewProfileCustomisation extends AppCompatActivity {
         // - Change font used in profile view
         // Set dropdown and its hints
 
+        // Init spinner for font change
+        // Font spinner drop down list
+        fontSpinner = (Spinner)findViewById(R.id.FontDropdown);
+        ArrayAdapter<String>fontArrayAdapter = new ArrayAdapter<String>(
+                ViewProfileCustomisation.this,
+                android.R.layout.simple_spinner_item,
+                fontDropdownOptions);
 
+        fontArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fontSpinner.setAdapter(fontArrayAdapter);
+        fontSpinner.setOnItemSelectedListener(this);
+
+
+
+
+
+
+        // - Change the accent theme of their own profile
+        accentThemeSpinner = (Spinner) findViewById(R.id.ChangeAccentThemeDropdown1);
+        ArrayAdapter<String>accentThemeArrayAdapter = new ArrayAdapter<String>(
+                ViewProfileCustomisation.this,
+                android.R.layout.simple_spinner_item,
+                accentThemeDropdownOptions);
+
+        accentThemeArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accentThemeSpinner.setAdapter(accentThemeArrayAdapter);
+        accentThemeSpinner.setOnItemSelectedListener(this);
+    }
+
+    // For spinner dropdown lists
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        String text = adapterView.getItemAtPosition(position).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // None
     }
 }
